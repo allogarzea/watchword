a40e283d7de59fedd260d88b7ea90458080b4706
Gaztea <heygaztea@proton.me>
2023-01-29 16:41:28 -0300

    feat: improve password output log

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,16 +4,17 @@ import { PasswordGenerator } from "./interface";
 import { validateParams } from "./validation/check";
 
 const log = console.log;
+const { gray, bold } = chalk;
 
 const getPassword = (passwordParams: PasswordGenerator) => {
     const generatedPassword = createRandomPass(passwordParams);
-    log(chalk.bold(generatedPassword));
+    log(`${gray('Generated Password:')} ${bold(generatedPassword)}`);
 }
 
 const getPasswordWithValidation = (passwordParams: PasswordGenerator) => {
     if (validateParams(passwordParams)) {
       const generatedPassword = createRandomPass(passwordParams);
-      log(chalk.bold(generatedPassword));
+      log(`${gray('Generated Password:')} ${bold(generatedPassword)}`);
     }
 }
 
