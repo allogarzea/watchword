576dbb1b482857a1832faedb31f44266a3fbe853
Gaztea <123768003+allogaztea@users.noreply.github.com>
2023-01-29 21:53:07 -0300

    Issue #10: Implemented Copy Password Feature

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import { clipboard } from "clipboard-sys";
 import { createRandomPass } from "./generate";
 import { PasswordGenerator } from "./interface";
 import { validateParams } from "./validation/check";
@@ -6,15 +7,22 @@ import { validateParams } from "./validation/check";
 const log = console.log;
 const { gray, bold } = chalk;
 
+const copyPassword = (password: string) => {
+    clipboard.writeText(password);
+    log(gray('Your password was copied to clipboard'));
+}
+
 const getPassword = (passwordParams: PasswordGenerator) => {
     const generatedPassword = createRandomPass(passwordParams);
     log(`${gray('Generated Password:')} ${bold(generatedPassword)}`);
+    copyPassword(generatedPassword);
 }
 
 const getPasswordWithValidation = (passwordParams: PasswordGenerator) => {
     if (validateParams(passwordParams)) {
       const generatedPassword = createRandomPass(passwordParams);
       log(`${gray('Generated Password:')} ${bold(generatedPassword)}`);
+      copyPassword(generatedPassword);
     }
 }
 
