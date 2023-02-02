f8788d531e0fd6b6e51dea702d0054f54f7beebc
Gaztea <heygaztea@proton.me>
2023-02-02 15:06:42 -0300

    feat: prepared lib to first release

---
+++ bin/watchword.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const bootstrap_1 = __importDefault(require("../commands/bootstrap"));
+(0, bootstrap_1.default)();
